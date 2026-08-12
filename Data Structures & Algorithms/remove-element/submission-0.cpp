09290af8fecfe7573144713320f6411ef0eef9d4
Agrim_47 <agrim77.com@gmail.com>
2026-08-13 03:24:24 +0530

    Add: remove-element - submission-0

---
+++ Data Structures & Algorithms/remove-element/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+
+        vector <int> ans;
+
+        for(int i =0;i<nums.size();i++){
+
+            if( nums[i] != val){
+
+                ans.push_back(nums[i]);
+            }
+            else{
+                continue;
+            }
+        }
+        
+        for(int i =0;i<ans.size();i++){
+
+            nums[i] = ans[i];
+        }
+        return ans.size();
+        
+    }
+};
