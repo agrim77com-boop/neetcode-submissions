27c32bbd94d634c8e80515e0d6b65d5367e881bf
Agrim_47 <agrim77.com@gmail.com>
2026-08-15 00:53:19 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+
+        unordered_map <int,int> mp;
+        vector <int> ans;
+
+        for(int i =0;i<nums.size();i++){
+
+            mp[nums[i]]++;
+
+            if(mp[nums[i]] == nums.size()/3 + 1){
+
+                ans.push_back(nums[i]);
+            }
+        }
+        return ans;
+    }
+};
