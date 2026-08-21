5740dbc6096f85ec03aeec35cd0014318ea67470
Agrim_47 <agrim77.com@gmail.com>
2026-08-22 02:27:09 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        unordered_map<char, int> freq;
+
+        for (int i = 0; i < s.length(); i++) {
+            freq[s[i]]++;
+            freq[t[i]]--;
+        }
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            if (freq[ch] != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
